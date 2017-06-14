20fafc831310725d7106a3d30607b88a485b972c
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2017-06-14 20:20:27 +0200

    bump

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
-
 const gulp = require('gulp'),
 	bump = require('gulp-bump');
 
 
 gulp.task('bump', () => {
 	return gulp.src('./package.json')
-	.pipe(bump())
-	.pipe(gulp.dest('.'));
+		.pipe(bump())
+		.pipe(gulp.dest('.'));
 });
 
+gulp.task('bump:minor', () => {
+	return gulp.src('./package.json')
+		.pipe(bump({
+			type: 'minor'
+		})).pipe(gulp.dest('.'));
+});
