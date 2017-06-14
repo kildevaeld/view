73221be788f3af71ee19c16d76166f2ca0e767b7
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2017-06-14 16:13:55 +0200

    removed test export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,4 +7,3 @@ export * from './base-view';
 export * from './utils';
 import { IView } from './types';
 export declare function mount<T extends IView>(el: Element, mountable: new (...args: any[]) => T): T;
-export * from './test';
