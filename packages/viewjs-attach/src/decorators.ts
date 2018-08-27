3bd516cef21200db0c0f953e85d4ee6bef979914
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-08-27 22:37:05 +0200

    adds more convience options to decorators

--- packages/viewjs-attach/src/decorators.ts
+++ packages/viewjs-attach/src/decorators.ts
@@ -1,10 +1,19 @@
-import { IView } from '@viewjs/view';
+import { IView, AbstractView } from '@viewjs/view';
 import { Constructor, Condition } from '@viewjs/types';
 import { IComponentView, AttachOptions } from './types';
-import { Resolvable } from './resolver';
-import { Invoker, isString } from '@viewjs/utils'
+import { Resolvable, Resolver } from './resolver';
+import { Invoker, isString, isFunction, isDestroyable } from '@viewjs/utils'
 
 const ComponentsKey = Symbol('view@attachments');
+
+
+export function isIView(a: any): a is IView {
+    return isFunction(a.render) && isDestroyable(a);
+}
+
+export function isIViewResolvable(a: any): a is Resolvable<IView> {
+    return isIView(a) || isFunction(a);
+}
 
 export interface DecoratorAttachOptions {
     view?: Resolvable<IView>
@@ -14,13 +23,15 @@ export interface DecoratorAttachOptions {
     condition?: Condition<IView>
 }
 
-export function attach(selector: string | DecoratorAttachOptions, options?: DecoratorAttachOptions) {
+export function attach(selector: string | DecoratorAttachOptions | Resolvable<IView>, options?: DecoratorAttachOptions) {
     return function <T extends IComponentView>(target: T, prop: string) {
 
-        if (!isString(selector)) {
-            options = selector;
-        } else {
+        if (isIViewResolvable(selector)) {
+            options = { view: selector };
+        } else if (isString(selector)) {
             (options || (options = {}))!.selector = selector;
+        } else {
+            options = selector;
         }
 
         options = Object.assign({
@@ -35,6 +46,7 @@ export function attach(selector: string | DecoratorAttachOptions, options?: Deco
         if (!options.view) throw new Error(`design:type does not exists for prop '${prop}' on '${target}'`);
 
         let meta = Reflect.getOwnMetadata(ComponentsKey, target);
+
         if (!meta) {
             meta = [];
             Reflect.defineMetadata(ComponentsKey, meta, target);
@@ -52,27 +64,39 @@ export function attach(selector: string | DecoratorAttachOptions, options?: Deco
     }
 }
 
-export type AttachmentsOptions = { [key: string]: AttachOptions }
+export interface AttachmentsAttachOptions {
+    view: Resolvable<IView>
+    optional?: boolean;
+    selector: string;
+    name?: string;
+    condition?: Condition<IView>
+}
+
+export type AttachmentsOptions = { [key: string]: AttachmentsAttachOptions | Resolvable<IView> };
 
 export function attachments(options: AttachmentsOptions) {
     return function <T extends IComponentView>(target: Constructor<T>) {
 
         let meta = Reflect.getOwnMetadata(ComponentsKey, target.prototype);
+
         if (!meta) {
             meta = [];
             Reflect.defineMetadata(ComponentsKey, meta, target.prototype);
         }
-
+        let v: any;
         for (let k in options) {
-            var v = options[k];
+            v = options[k];
+            if (isIView(v) || isFunction(v)) {
+                v = { view: v };
+            }
             meta.push(Object.assign({ name: k }, v));
         }
-
     }
 }
 
 
 export function componentFromMeta(view: IView): AttachOptions[] {
     let out = Reflect.getMetadata(ComponentsKey, view);
+
     return out || [];
 }
