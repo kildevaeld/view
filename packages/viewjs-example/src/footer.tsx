95cac3798e596dd12357e0318901100b31a3be39
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-10-17 13:37:09 +0200

    adds react to example

---
+++ packages/viewjs-example/src/footer.tsx
@@ -0,0 +1,26 @@
+import * as Preact from 'preact';
+import { PreactView } from '@viewjs/preact';
+
+const React = { createElement: Preact.h }
+
+
+export interface FooterProps {
+    title: string;
+}
+
+export class Footer extends Preact.Component<FooterProps, any> {
+
+
+
+    render() {
+        return <div>
+            <div>Footer: {this.props.title}</div>
+        </div>
+    }
+
+}
+
+
+export function create(model: FooterProps) {
+    return new PreactView(Footer, model);
+}
