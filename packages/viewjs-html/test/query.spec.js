64dc1a08ad59887bef41208e91d3df375bad6353
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-10-21 14:14:03 +0200

    adds more tests

--- packages/viewjs-html/test/query.spec.js
+++ packages/viewjs-html/test/query.spec.js
@@ -14,9 +14,44 @@ describe('Query', () => {
         console.log(viewjs.html.normalize(document))
     });
 
-    it('shold create element', () => {
+    it('should create element', () => {
         let el = viewjs.html.normalize('<div class="test"></div>');
         console.log(el);
     });
 
-})
+    it('should be a ierator', () => {
+        const context = document.createElement('div');
+        context.innerHTML = `<div>
+            <button class="btn1"></button>
+            <button class="btn2"></button>
+            <button class="btn3"></button>
+        </div>`;
+
+        let $e = viewjs.html.html('button', context);
+
+        let iter = $e[Symbol.iterator]();
+
+        expect(iter.next().value).to.not.equal(null);
+        expect(iter.next().value).to.not.equal(null);
+        expect(iter.next().value).to.not.equal(null);
+        expect(iter.next().value).to.equal(null);
+
+    })
+
+    it('should flattern html', () => {
+        const context = document.createElement('div');
+        context.innerHTML = `<div>
+            <button class="btn1"></button>
+            <button class="btn2"></button>
+            <button class="btn3"></button>
+        </div>`;
+
+
+        let e1 = viewjs.html.html('.btn1', context),
+            e2 = viewjs.html.html('.btn2', context);
+
+        console.log(e1, e2, viewjs.html.html([e1, e2]));
+
+    });
+
+});
