3af3f43d1c2c1044cf15b1d1bb09a1215a40db60
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2019-01-21 18:30:14 +0100

    export utils from view

--- packages/viewjs-view/src/index.ts
+++ packages/viewjs-view/src/index.ts
@@ -2,6 +2,7 @@ export * from './types';
 export * from './decorators';
 export * from './base-view';
 export * from './abstract-view';
+export * from './utils';
 // mixins
 export * from './with-template';
 export * from './with-element';
