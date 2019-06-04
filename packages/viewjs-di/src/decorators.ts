ea23fbf69bd7139f6736274e2f3e3580a60f9a5b
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2019-06-04 13:59:53 +0200

    adds property decorator

--- packages/viewjs-di/src/decorators.ts
+++ packages/viewjs-di/src/decorators.ts
@@ -1,6 +1,7 @@
 import { MetaKeys, IActivator, emptyParameters, IDependencyResolver } from './common';
 import { TransientRegistration, SingletonRegistration } from './registers';
 import { FactoryActivator } from './activators';
+import { global } from './container';
 
 
 /**
@@ -18,6 +19,55 @@ export function inject(...rest: any[]) {
             target.inject = rest;
     }
 }
+
+
+/**
+ * Mark a property to lazy injected by the global di-container,
+ * @param target 
+ * @param key 
+ * @param descriptor 
+ * @example
+ *  class SomeComponent extends Component {
+ *      @inject
+ *      service: Service
+ * 
+ *      render() {
+ *          return <div>{this.service.getAll()}</div>
+ *      }
+ *  }
+ */
+export function property(target: any, key: string, descriptor?: PropertyDescriptor): any {
+    let Dependency = Reflect.getOwnMetadata("design:type", target, key);
+    if (!Dependency) throw new Error('design:type metadata does not exists on property. consider compiling typescript with "emitDecoratorMetadata" enabled or add design:type metadata manually');
+
+    // In IE11 calling Object.defineProperty has a side-effect of evaluating the
+    // getter for the property which is being replaced. This causes infinite
+    // recursion and an "Out of stack space" error.
+    let definingProperty = false;
+
+    return {
+        configurable: true,
+        get: function () {
+            if (definingProperty || this === target.prototype || this.hasOwnProperty(key)) {
+                return descriptor;
+            }
+            // Cache the dependency resolution as we don't want transient values
+            // to keep being instantiated on property access.
+            const instance = global().get(Dependency);
+            definingProperty = true;
+            Object.defineProperty(this, key, {
+                get() {
+                    return instance;
+                }
+            });
+            definingProperty = false;
+            return instance;
+        },
+    }
+
+}
+
+
 
 export function registration(value: any, targetKey?: string) {
     return function (target: any) {
