aa7860a2a3ce636c11756dd276d0fbe4c99dace1
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2019-07-15 14:49:37 +0200

    import reflect-metadata

--- packages/viewjs-di/rollup.config.js
+++ packages/viewjs-di/rollup.config.js
@@ -1,32 +1,37 @@
 const resolve = require('rollup-plugin-node-resolve'),
-    commonjs = require('rollup-plugin-commonjs'),
-    babel = require('rollup-plugin-babel'),
-    typescript = require('rollup-plugin-typescript');
+      commonjs = require('rollup-plugin-commonjs'),
+      babel = require('rollup-plugin-babel'),
+      typescript = require('rollup-plugin-typescript');
 
 const pkg = require('./package.json');
 
 module.exports = [
-    // browser-friendly UMD build
-    {
-        input: './src/index.ts',
-        output: [{
-            file: pkg.browser,
-            format: 'umd',
-            name: 'viewjs.di'
-        }, {
-            file: pkg.module,
-            format: 'es',
-        }],
-        plugins: [
-            typescript({
-                exclude: ['node_modules/**'],
-                typescript: require('typescript'),
-                declaration: false,
-                module: 'es2015'
-            }),
-            babel({
-                exclude: ['node_modules/**']
-            })
-        ]
-    }
+  // browser-friendly UMD build
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: pkg.browser,
+        format: 'umd',
+        name: 'viewjs.di',
+        globals: {
+          'reflect-metadata': 'Reflect',
+        }
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      }
+    ],
+    external: ['reflect-metadata'],
+    plugins: [
+      typescript({
+        exclude: ['node_modules/**'],
+        typescript: require('typescript'),
+        declaration: false,
+        module: 'es2015'
+      }),
+      babel({exclude: ['node_modules/**']})
+    ]
+  }
 ];
