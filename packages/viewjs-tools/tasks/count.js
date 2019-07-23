67ece9bf1b09209a87ec7685ccfbd305a40a66ea
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2019-07-23 20:16:07 +0200

    print total lines for individual packages

--- packages/viewjs-tools/tasks/count.js
+++ packages/viewjs-tools/tasks/count.js
@@ -1,23 +1,22 @@
-const
-    fs = require('mz/fs'),
-    Path = require('path');
+const fs = require('mz/fs'), Path = require('path');
 
 
 async function count() {
-    const path = Path.join(__dirname, "../..");
-    const models = await fs.readdir(path);
+  const path = Path.join(__dirname, '../..');
+  const models = await fs.readdir(path);
 
-    let lines = 0;
-    for (let model of models) {
-        const p = Path.join(path, model, `dist/${model.replace("-", '.')}.es6`);
-        if (await fs.exists(p)) {
-            let buffer = await fs.readFile(p, 'utf8');
-            lines += (buffer.match(/\r?\n/g) || '').length + 1
-        }
+  let totalLines = 0;
+  for (let model of models) {
+    const p = Path.join(path, model, `dist/${model.replace('-', '.')}.es6`);
+    if (await fs.exists(p)) {
+      let buffer = await fs.readFile(p, 'utf8');
+      let lines = (buffer.match(/\r?\n/g) || '').length + 1;
+      console.log(`${model}\t\t${lines}`);
+      totalLines += lines;
     }
+  }
 
-    console.log('lines', lines);
-
+  console.log('Total', totalLines);
 }
 
 count()
